e28b100dd04f230323ec0ebc886c6133f797a8ea
Ikechukwu Chiemelie Charles <chiemeliecharleschristopher@gmail.com>
2024-11-23 07:29:49 +0100

    implemented drop down menu and like button

--- Pages/data.js
+++ Pages/data.js
@@ -1,24 +1,69 @@
-"use strict";
+// "use strict";
 
-const hamburger = document.querySelector(".ham");
+const hamburger = document.querySelectorAll(".ham");
 const closeBtn = document.querySelector(".close");
 const menu = document.querySelector(".mobileMenu");
 
-function openMenu() {
-  hamburger.addEventListener("click", function () {
-    menu.classList.toggle("hidden");
-    console.log("clicked open");
-  });
-}
-openMenu();
+//like icon animation
+const likeBtns = document.getElementsByClassName("liked");
 
+const commentBtns = document.querySelectorAll(".commented");
+const shareBtns = document.querySelectorAll(".shared");
+
+//drop down menu
+const dropDownBtn = document.querySelector(".selectButton");
+const dropDownMenu = document.querySelector(".options");
+
+const options = document.querySelectorAll(".option");
+
+//open harmburger
+
+function openMenu() {
+  menu.classList.toggle("hidden");
+}
+for (const item of hamburger) {
+  item.addEventListener("click", openMenu);
+}
+
+//Close harmburger
 function closeMenu() {
-  closeBtn.addEventListener("click", function () {
-    menu.classList.toggle("hidden");
-    console.log("clicked close");
+  menu.classList.toggle("hidden");
+}
+
+for (const item of hamburger) {
+  closeBtn.addEventListener("click", closeMenu);
+}
+
+//Changing icon color
+function iconColor() {
+  this.classList.toggle("clicked");
+}
+function removeColor() {
+  this.classList.toggle("unclicked");
+}
+
+for (const items of likeBtns) {
+  items.addEventListener("click", iconColor);
+}
+for (const items of commentBtns) {
+  items.addEventListener("click", iconColor);
+}
+for (const items of shareBtns) {
+  items.addEventListener("click", iconColor);
+}
+
+//dropDown Menu
+dropDownBtn.addEventListener("click", function () {
+  dropDownMenu.classList.toggle("hide");
+});
+
+for (const items of options) {
+  items.addEventListener("click", function () {
+    dropDownBtn.textContent = items.textContent;
+    dropDownMenu.classList.toggle("hide");
+    dropdown.classList.toggle("open");
   });
 }
-closeMenu();
 
 // hamburger.addEventListener("click", function () {
 //   if (!menu.classList.contains("open")) {
